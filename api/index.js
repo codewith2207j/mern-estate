cd3ea9603dcf77a4b4f9129e117b71ff8f3538c9
codewith2207j <codewith2207j@gmail.com>
2024-04-14 17:48:26 +0530

    connected to DB

--- api/index.js
+++ api/index.js
@@ -1,8 +1,19 @@
 import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 const app = express();
 const PORT = 4000;
-
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
-});
+mongoose
+  .connect(process.env.MONDODB_URL)
+  .then(() => {
+    console.log(`DB connected`);
+    app.listen(PORT, () => {
+      console.log(`Server is running on port ${PORT}`);
+    });
+  })
+  .catch((err) => {
+    console.log("error: ", err);
+  });
